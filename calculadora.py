9daaebd0207e032a7000965d7f9235041be4d8f2
Scarro <sergioca22@gmail.com>
2017-02-07 15:10:16 +0100

    actualizado

--- calculadora.py
+++ calculadora.py
@@ -1,41 +1,38 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Sergio Carro Albarrán
+#Calculadora simple en python
 
 import sys
 
-
 def sumar(operando1, operando2):
-    print(operando1 + operando2)
-
+	return operando1 + operando2
 
 def restar(operando1, operando2):
-    print(operando1 - operando2)
-
+	return operando1 - operando2
 
 def multiplicar(operando1, operando2):
-    print(operando1 * operando2)
-
+	return operando1 * operando2
 
 def dividir(operando1, operando2):
-    try:
-        print(operando1/operando2)
-    except ZeroDivisionError:
-        sys.exit("Imposible dividir entre 0")
+	return operando1 / operando2
 
 if __name__ == "__main__":
-    try:
-        operando1 = float(sys.argv[2])
-        operando2 = float(sys.argv[3])
-        if sys.argv[1] == "sumar":
-            sumar(operando1, operando2)
-        elif sys.argv[1] == "restar":
-            restar(operando1, operando2)
-        elif sys.argv[1] == "multiplicar":
-            multiplicar(operando1, operando2)
-        elif sys.argv[2] == "dividir":
-            dividir(operando1, operando2)
-        else:
-            print ("Uso: Operaciones disponibles: +  -  *  /")
-    except ValueError:
-        sys.exit("Al menos uno de los operandos no es un numero")
-    except IndexError:
-        sys.exit("Uso: python calculadora.py funcion operando1 operando2")
+	try:
+		if len(sys.argv) != 4:
+			raise IndexError(len(sys.argv)-1)
+		funcion = str(sys.argv[1])
+		operando1 = float(sys.argv[2])
+		operando2 = float(sys.argv[3])
+		print locals()[funcion](operando1, operando2)
+	except ValueError:
+		sys.exit("Al menos uno de los operandos no es un número")
+	except IndexError as e:
+		mensaje = "Uso: python calculadora.py funcion operando1 operando2"
+		sys.exit('(' + str(e.args[0]) + ') ' + mensaje)
+	except KeyError as e:
+		print(str(e.args[0]) + " no es una función permitida")
+		sys.exit("Operaciones: sumar, restar, multiplicar, dividir")
+	except ZeroDivisionError:
+		sys.exit("Imposible dividir entre 0");
